b1a20471224458336c89e6ff88e8ada603c2657c
AryoBama <13523088@std.stei.itb.ac.id>
2026-01-12 18:30:11 +0700

    fix: Fix depedencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,10 @@ plugins {
 
 android {
     namespace = "com.bamane.myfinance"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.bamane.myfinance"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
@@ -31,11 +29,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
