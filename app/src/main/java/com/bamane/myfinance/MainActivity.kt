6f510d4f4b75f38825504f8425fa3d5d6be211ea
AryoBama <13523088@std.stei.itb.ac.id>
2026-01-20 21:24:59 +0700

    feat: integrate dashboard

--- app/src/main/java/com/bamane/myfinance/MainActivity.kt
+++ app/src/main/java/com/bamane/myfinance/MainActivity.kt
@@ -12,36 +12,37 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.bamane.myfinance.core.designsystem.MyApplicationTheme
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.graphics.Color
+import com.bamane.myfinance.feature.dashboard.DashboardScreen
+import com.bamane.myfinance.feature.friend.FriendScreen
 
 class MainActivity : ComponentActivity() {
+    val bgColor = Color(0xFFF7F8FF)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Test",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) { padding ->
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(padding)
+                            .background(bgColor),
+                        color = Color(0xFFF7F8FF),
+                        contentColor = Color.Black
+
+                    ) {
+                        DashboardScreen()
+                    }
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
     }
 }
