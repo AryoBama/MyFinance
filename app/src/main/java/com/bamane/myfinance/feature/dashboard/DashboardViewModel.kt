ce68908a30095b5e68a350cb6e7999db80fa5522
AryoBama <13523088@std.stei.itb.ac.id>
2026-01-20 21:24:43 +0700

    feat: add dashboard view model

---
+++ app/src/main/java/com/bamane/myfinance/feature/dashboard/DashboardViewModel.kt
@@ -0,0 +1,59 @@
+package com.bamane.myfinance.feature.dashboard
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.bamane.myfinance.MyApplication
+import com.bamane.myfinance.core.data.FinanceRepository
+import com.bamane.myfinance.core.model.BillPreviewModel
+import com.bamane.myfinance.feature.friend.FriendViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+
+const val BILL_LIMIT = 3
+class DashboardViewModel(private val repository: FinanceRepository): ViewModel() {
+
+    val totalReceivable: StateFlow<Double> = repository.totalReceivable
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = 0.0
+        )
+
+    val totalDebt: StateFlow<Double> = repository.totalDebt
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = 0.0
+        )
+
+    val totalBalance: StateFlow<Double> =
+        combine(totalReceivable, totalDebt) { receivable, debt ->
+            receivable - debt
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = 0.0
+        )
+
+    val billPreviews: StateFlow<List<BillPreviewModel>> = repository.getBillPreviews(BILL_LIMIT)
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = emptyList()
+        )
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MyApplication)
+                val repository = application.repository
+                DashboardViewModel(repository)
+            }
+        }
+    }
+}
